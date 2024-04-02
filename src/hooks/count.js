dd4f86bcd2d2a6c35058913eea167ced844ecbeb
Kamran Aslam <kamraslam184@gmail.com>
2024-04-02 15:22:52 +0500

    counter hook comments added

--- src/hooks/count.js
+++ src/hooks/count.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
+// custom hooks name must start with use keyword e.g useCounter
 export function useCount() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
 
   function increment() {
     setCount(count + 1);
   }
 
+  // returns only the declared variable and functions
   //   return {count,increment}
 
+  // returns variables as an array of {count,increment} can be used with any variable name as parameter
   return [count, increment];
   //   [0,1]
 }
